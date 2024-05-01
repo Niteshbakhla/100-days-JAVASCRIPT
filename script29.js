5dc1915383cec5485fa676ceb99714faad731d85
Nitesh bakhla <143624783+Niteshbakhla@users.noreply.github.com>
2024-05-01 22:38:46 +0530

    generate a number with the help of function

---
+++ script29.js
@@ -0,0 +1,14 @@
+const rangeOfNumbers = (a, b) => {
+            let val = []
+            for (let i = a; i <= b; i++) {
+                        val.push(i)
+            }
+
+            return val
+}
+
+console.log(rangeOfNumbers(0, 500));
+
+
+
+
